b676c0a465a52490406d630460e27e2bb7a0f993
indy koning <indy@justbetter.nl>
2023-09-05 17:12:28 +0200

    Remove unnecessary push of empty ecommerce object

--- resources/js/datalayer/ga4.js
+++ resources/js/datalayer/ga4.js
@@ -39,7 +39,6 @@ export const login = async () => {
 
 export const search = async (searchTerm) => {
     // https://developers.google.com/analytics/devguides/collection/ga4/reference/events?client_type=gtm#search
-    dataLayer.push({ ecommerce: null })
     dataLayer.push({
         event: 'search',
         search_term: searchTerm,
