7981d6f1477a0c8aad4d729dd34f102b85f25f37
indykoning <15870933+indykoning@users.noreply.github.com>
2023-11-30 11:10:00 +0100

    Move config file to Rapidez folder (#14)

--- resources/views/partytown/config.blade.php
+++ resources/views/partytown/config.blade.php
@@ -4,14 +4,14 @@
         forward: [],
         debug: {{ var_export(config('app.debug'), true) }},
         resolveUrl: function (url, location, type) {
-            if (@json(config('rapidez-gtm.partytown.domain_whitelist')).includes(url.host)) {
+            if (@json(config('rapidez.gtm.partytown.domain_whitelist')).includes(url.host)) {
                 return @json(route('rapidez-gtm::proxy', ['url' => '/'], false) . '/') + url.href;
             }
             return url
         }
     }
 
-    @if(config('rapidez-gtm.partytown.enabled'))
+    @if(config('rapidez.gtm.partytown.enabled'))
         partytown.forward.push('dataLayer.push');
     @endif
 </script>
