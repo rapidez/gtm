5bce3c41560e1c33b1b0f581343332e1547c2047
indy koning <indy@justbetter.nl>
2024-04-22 15:54:48 +0200

    Support tag assistant debugger in Partytown

--- resources/views/partytown/config.blade.php
+++ resources/views/partytown/config.blade.php
@@ -1,7 +1,50 @@
 @once
 <script>
+    @if(config('rapidez.gtm.partytown.enabled'))
+    if (window.__TAG_ASSISTANT_API !== undefined) {
+        // Tag assistant compatibility
+        const gtmDebugLog = (msg, data) => {
+            if (window.partytown.debug) {
+                console.debug(
+                `%cGTM Main%c${msg}`,
+                `background: #c47ed1; color: white; padding: 2px 3px; border-radius: 2px; font-size: 0.8em;margin-right:5px`,
+                `background: #999999; color: white; padding: 2px 3px; border-radius: 2px; font-size: 0.8em;`,
+                data && data.length ? Array.from(data) : data
+                )
+            }
+        }
+
+        __tag_assistant_accessor = {
+            receiver: function(...args) {
+                window.__tag_assistant_forwarder.apply(null, arguments);
+            },
+
+            // Called when receiver has been set inside partytown, calls __TAG_ASSISTANT_API.setReceiver
+            setReceiver: function () {
+                gtmDebugLog('activate')
+                window.__TAG_ASSISTANT_API.setReceiver(function() {
+                    gtmDebugLog('send data', arguments)
+                    window.__tag_assistant_forwarder.apply(null, arguments);
+                })
+            },
+
+            // Forwards calls from bootstrap
+            sendMessage: function() {
+                gtmDebugLog('send message', arguments)
+                window.__TAG_ASSISTANT_API.sendMessage.apply(window.__TAG_ASSISTANT_API, arguments)
+            },
+            // Forwards calls from bootstrap
+            disconnect: function() {
+                gtmDebugLog('disconnect', arguments)
+                window.__TAG_ASSISTANT_API.disconnect.apply(window.__TAG_ASSISTANT_API, arguments)
+            },
+        }
+    }
+    @endif
+
     partytown = {
         forward: [],
+        mainWindowAccessors: [],
         debug: {{ var_export(config('app.debug'), true) }},
         resolveUrl: function (url, location, type) {
             if (@json(config('rapidez.gtm.partytown.domain_whitelist')).includes(url.host)) {
@@ -12,7 +55,26 @@
     }
 
     @if(config('rapidez.gtm.partytown.enabled'))
-        partytown.forward.push('dataLayer.push');
+        partytown.forward.push(['dataLayer.push', { preserveBehavior: true }]);
+        partytown.forward.push('__tag_assistant_forwarder');
+        partytown.mainWindowAccessors.push('__tag_assistant_accessor');
     @endif
 </script>
+@if(config('rapidez.gtm.partytown.enabled'))
+<script type="text/partytown">
+    window.__TAG_ASSISTANT_API = window.__TAG_ASSISTANT_API || Object.assign({}, window.__tag_assistant_accessor, {
+        // Override setReceiver to enable main <-> worker communication
+        setReceiver: (receiver) => {
+            // The receiving function is assigned to `window[tagAssistantForwarderName]`,
+            // allowing it to be called from the main thread.
+            // @ts-ignore
+            window.__tag_assistant_forwarder = receiver;
+
+            // The original setReceiver function is called to notify the main thread that
+            // the receiver has been set.
+            window.__tag_assistant_accessor?.setReceiver();
+        }
+    });
+</script>
+@endif
 @endonce
